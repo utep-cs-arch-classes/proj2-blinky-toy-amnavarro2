bf5f7285ae9320a3be39630df086cdad418f75b6
Alexis <amnavarro2@miners.utep.edu>
2021-04-12 19:16:16 -0600

    moved some files into the blink_demo folder

---
+++ project/blink_demo/stateMachines.h
@@ -0,0 +1,10 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+void red_dim();
+void green_dim();
+void red_dim_25();
+void green_dim_25();
+void change_dimming();
+#endif // included
