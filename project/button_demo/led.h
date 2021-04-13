b4cc50e77b4655c19842a13ff034445819785b3d
Alexis <amnavarro2@miners.utep.edu>
2021-04-13 15:16:44 -0600

    removed variables not being used

--- project/button_demo/led.h
+++ project/button_demo/led.h
@@ -12,6 +12,5 @@ void led_update();		/* update leds */
 
 // these are boolean flags
 extern unsigned char red_on, green_on, led_changed;
-extern unsigned char leds_changed, green_led_state, red_led_state;
 
 #endif
