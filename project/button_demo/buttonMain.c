b70d73aa49731ee7a99d7befaa2ee2b0997f3d12
Alexis <amnavarro2@miners.utep.edu>
2021-04-13 14:52:48 -0600

    updating all the files

--- project/button_demo/buttonMain.c
+++ project/button_demo/buttonMain.c
@@ -2,6 +2,7 @@
 #include "libTimer.h"
 #include "led.h"
 #include "switches.h"
+#include "stateMachines.h"
 
 void main(void) 
 {  
@@ -9,5 +10,6 @@ void main(void)
   switch_init();
   led_init();
 
+  enableWDTInterrupts();
   or_sr(0x18);  // CPU off, GIE on
 } 
