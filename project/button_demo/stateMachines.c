2493e82c0a0a26ff321a4f2754e0cb672316dc11
Alexis <amnavarro2@miners.utep.edu>
2021-04-13 15:51:11 -0600

    fixed minor issues in comments

--- project/button_demo/stateMachines.c
+++ project/button_demo/stateMachines.c
@@ -2,7 +2,7 @@
 #include "stateMachines.h"
 #include "led.h"
 
-static char bright_state = 0;
+//static char bright_state = 0;
 
 char toggle_red()		/* always toggle! */
 {
@@ -21,22 +21,21 @@ char toggle_red()		/* always toggle! */
   return 1;			/* always changes an led */
 }
 
-//Turns on the red only
 void toggle_red_on(){
-  red_on = 1;
+  red_on =1;
+
+  led_changed = 1;
+  led_update();
+
+}//end
+
+void toggle_red_off(){
+  red_on = 0;
   
   led_changed = 1;
   led_update();
 }//end
 
-//turns of the red only
-void toggle_red_off(){
-  red_on = 0;
-
-  led_changed = 1;
-  led_update();
-
-}//end
 
 char toggle_green()	/* only toggle green if red is on!  */
 {
@@ -48,24 +47,22 @@ char toggle_green()	/* only toggle green if red is on!  */
   return changed;
 }
 
-//turns on the green only
+
 void toggle_green_on(){
   green_on = 1;
 
-  led_changed =1;
+  led_changed = 1;
   led_update();
-}//end
+}
 
 
-//turns off the green only
 void toggle_green_off(){
-  green_on =0;
+  green_on = 0;
 
   led_changed = 1;
   led_update();
-}//end
 
-
+}
 
 void state_advance()		/* alternate between toggling red & green */
 {
@@ -81,7 +78,7 @@ void state_advance()		/* alternate between toggling red & green */
   led_update();
 }
 
-//this method dims red 75 percent
+//red 75 brightness
 void red_dim(){
   static char state = 0;
 
@@ -104,7 +101,7 @@ void red_dim(){
   led_update();
 }//end red_dim
 
-//this method dims green 75 percent
+//green 75 brightness
   void green_dim(){
     static char state = 0;
 
@@ -127,7 +124,7 @@ void red_dim(){
   led_update();
 }//end green_dim
 
-//this method dims red 25 percent
+//25 red brightness
 void red_dim_25(){
   static char state =0;
 
@@ -153,7 +150,8 @@ void red_dim_25(){
   led_update();
 }//end red_dim_25
 
-//this method dims green 25 percent
+
+//25 green brightness
 void green_dim_25(){
   static char state = 0;
 
@@ -179,34 +177,34 @@ void green_dim_25(){
   led_update();
 }//end green_dim_25
 
-
-//assign instructions for what happens in each state for dimming 
+//assign instructions for what happens in each state
 void state_dimming(){
+  //static char state = 0;
 
   switch(bright_state){
   case 0:
     toggle_red_on();
-    //toggle_green();
+    toggle_green_on();
     //state = 1;
     break;
   case 1:
     red_dim();
-    //green_dim();
+    green_dim();
     //state = 2;
     break;
   case 2:
     red_dim_25();
-    //green_dim_25();
-    // state = 3;
+    green_dim_25();
+    //state = 0;
     break;
   case 3:
     toggle_red_off();
-    //state = 0;
+    toggle_green_on();
     break;
   }//end switch
-}//end change_dimming
-
-
+  led_changed = 1;
+  led_update();
+}//end state_dimming
 
 //access the states from the method above each time we move states.
 void change_states(){
@@ -226,3 +224,4 @@ void change_states(){
   }//end switch
   led_update();
 }//end change_states
+
