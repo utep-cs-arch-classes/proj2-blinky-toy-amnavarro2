f49275238fcf41cb0818c23b403a736808ffccac
Alexis <amnavarro2@miners.utep.edu>
2021-04-13 15:51:40 -0600

    fixed issues and added a variable

--- project/button_demo/stateMachines.h
+++ project/button_demo/stateMachines.h
@@ -2,17 +2,19 @@
 #define stateMachine_included
 
 void state_advance();
+
 void toggle_red_on();
 void toggle_red_off();
-
 void toggle_green_on();
-void toggle_green_off();
+void toggled_green_off();
 
 void red_dim();
 void green_dim();
 void red_dim_25();
 void green_dim_25();
-void change_dimming();
+
 void state_dimming();
 void change_states();
+
+extern int bright_state;
 #endif // included
